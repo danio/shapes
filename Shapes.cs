9b9345993655e2492e9ea92b31935bd8bc3d48da
Dan Pidcock <dan.pidcock@googlemail.com>
2014-09-14 20:36:37 +0100

    initial project setup

---
+++ Shapes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shapes
+{
+    class Shapes
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0}, {1}, {2}",
+                "hello",
+                "c#",
+                "app");
+        }
+    }
+}
