e8f09e4352e5eecdc60f4c58a85cf94832af975f
Dan Pidcock <dan.pidcock@googlemail.com>
2014-09-14 21:57:17 +0100

    Added circle

---
+++ Circle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shapes
+{
+    class Circle : IWidget
+    {
+        Vector _location;
+        int _diameter;
+
+        public Circle(Vector location, int diameter)
+        {
+            _location = new Vector(location);
+            _diameter = diameter;
+        }
+
+        // convenience constructor interface can be used if preferred
+        public Circle(int x, int y, int diameter)
+            : this(new Vector(x, y), diameter)
+        {
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1} size={2}", this.GetType().Name, _location, _diameter);
+        }
+    }}
--- Shapes.cs
+++ Shapes.cs
@@ -15,6 +15,7 @@ namespace shapes
             drawing.Add(new Rectangle(location: new Vector(10, 10), width: 30, height: 40));
             drawing.Add(new Square(location: new Vector(15, 30), size: 35));
             drawing.Add(new Ellipse(location: new Vector(100, 150), diameterH: 300, diameterV: 200));
+            drawing.Add(new Circle(location: new Vector(1, 1), diameter: 300));
 
             Console.WriteLine(drawing);
         }
