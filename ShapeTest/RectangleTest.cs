ca7a72c1f413d725be5980a9e9e2a6d129a2325f
Dan Pidcock <dan.pidcock@googlemail.com>
2014-09-14 22:58:15 +0100

    Rectangle unit tests

---
+++ ShapeTest/RectangleTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using shapes;
+
+namespace ShapeTest
+{
+    [TestClass]
+    public class RectangleTest
+    {
+        [TestMethod]
+        public void ConvenienceConstructorsShouldHaveSameEffectAsLocationConstructor()
+        {
+            Vector location = new Vector(3, 7);
+            Vector size = new Vector(42, 77);
+            Rectangle standardConstructed = new Rectangle(location, size);
+            Rectangle convenienceConstructed1 = new Rectangle(location, size.X, size.Y);
+            Rectangle convenienceConstructed2 = new Rectangle(location.X, location.Y, size.X, size.Y);
+
+            Assert.AreEqual(standardConstructed.ToString(), convenienceConstructed1.ToString());
+            Assert.AreEqual(standardConstructed.ToString(), convenienceConstructed2.ToString());
+        }
+    }
+}
