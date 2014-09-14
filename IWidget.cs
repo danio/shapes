64ea536c1b97b98d89f621ddd752c271ab1bf406
Dan Pidcock <dan.pidcock@googlemail.com>
2014-09-14 21:59:56 +0100

    Remove redundant usings

--- IWidget.cs
+++ IWidget.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace shapes
+﻿namespace shapes
 {
     interface IWidget
     {
