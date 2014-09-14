c9e6ec8ae09d0b15110fc381c247bda848cfce52
Dan Pidcock <dan.pidcock@googlemail.com>
2014-09-14 22:04:34 +0100

    Added Textbox

--- Shapes.cs
+++ Shapes.cs
@@ -12,6 +12,7 @@ namespace shapes
             drawing.Add(new Square(location: new Vector(15, 30), size: 35));
             drawing.Add(new Ellipse(location: new Vector(100, 150), diameterH: 300, diameterV: 200));
             drawing.Add(new Circle(location: new Vector(1, 1), diameter: 300));
+            drawing.Add(new Textbox(location: new Vector(5, 5), width: 200, height: 100, text: "sample text"));
 
             Console.WriteLine(drawing);
         }
---
+++ Textbox.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shapes
+{
+    class Textbox : IWidget
+    {
+        Vector _location;
+        Vector _size; // width and height stored as a vector for easier manipulation/calculation
+        string _text;
+
+        public Textbox(Vector location, Vector size, string text)
+        {
+            _location = new Vector(location);
+            _size = new Vector(size);
+            _text = text;
+        }
+
+        // convenience constructor interface can be used if preferred
+        public Textbox(Vector location, int width, int height, string text)
+            : this(location, new Vector(width, height), text)
+        {
+        }
+
+        // convenience constructor interface can be used if preferred
+        public Textbox(int x, int y, int width, int height, string text)
+            : this(new Vector(x, y), width, height, text)
+        {
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1} width={2} height={3} Text=\"{4}\"", GetType().Name, _location, _size.X, _size.Y, _text);
+        }
+    }
+}
