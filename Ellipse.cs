5d4354ffc941a167a9a6adf713cbb1054120c1fc
Dan Pidcock <dan.pidcock@googlemail.com>
2014-09-14 21:51:03 +0100

    Added Ellipse

---
+++ Ellipse.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shapes
+{
+    class Ellipse : IWidget
+    {
+        Vector _location;
+        Vector _size; // horizontal and vertical diameter stored as a vector for easier manipulation/calculation
+
+        public Ellipse(Vector location, Vector size)
+        {
+            _location = new Vector(location);
+            _size = new Vector(size);
+        }
+
+        // convenience constructor interface can be used if preferred
+        public Ellipse(Vector location, int diameterH, int diameterV)
+            : this(location, new Vector(diameterH, diameterV))
+        {
+        }
+
+        // convenience constructor interface can be used if preferred
+        public Ellipse(int x, int y, int diameterH, int diameterV)
+            : this(new Vector(x, y), diameterH, diameterV)
+        {
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1} diameterH={2} diameterV={3}", GetType().Name, _location, _size.X, _size.Y);
+        }
+    }
+}
--- Shapes.cs
+++ Shapes.cs
@@ -14,6 +14,7 @@ namespace shapes
 
             drawing.Add(new Rectangle(new Vector(10, 10), 30, 40));
             drawing.Add(new Square(new Vector(15, 30), 35));
+            drawing.Add(new Ellipse(new Vector(100, 150), 300, 200));
 
             Console.WriteLine(drawing);
         }
