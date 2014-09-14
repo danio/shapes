9f6b783f2d44a36e45302c5f64d9142a31db81e3
Dan Pidcock <dan.pidcock@googlemail.com>
2014-09-14 22:51:42 +0100

    test square constructors are consistent

---
+++ ShapeTest/SquareTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using shapes;
+
+namespace ShapeTest
+{
+    [TestClass]
+    public class SquareTest
+    {
+        [TestMethod]
+        public void ConvenienceConstructorShouldHaveSameEffectAsLocationConstructor()
+        {
+            Vector location = new Vector(3,7);
+            int size = 42;
+            Square square1 = new Square(location, size);
+            Square square2 = new Square(location.X, location.Y, size);
+
+            Assert.AreEqual(square1.ToString(), square2.ToString());
+        }
+    }
+}
